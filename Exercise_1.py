46fe1a39e8b5610c55ac9ad567663f55f49cef0f
TimHuerzeler <94198396+TimHuerzeler@users.noreply.github.com>
2022-10-06 14:54:52 +0200

    added Exercises

---
+++ Exercise_1.py
@@ -0,0 +1,4 @@
+# Develop a function that returns True if its parameter is even, False otherwise.
+
+#Author: TimHuerzeler
+#Date: 06.10.2022
---
+++ Exercise_7.py
@@ -0,0 +1,4 @@
+# Develop a function that computes the area of a disc of a given radius.
+
+#Author: TimHuerzeler
+#Date: 06.10.2022
